a307406cb3ee6d930960f5859e05948b9c7eeb1b
murthychiluka <murthychiluka@gmail.com>
2023-10-14 14:46:52 +0200

    shell1

---
+++ firstscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#checking systemuptime
+echo "The uptime of system is":
+uptime
+echo
+#memory utilization
+echo "memory utilization"
+free -m
+echo
+#disk utilization
+
+echo
+echo "Disk utilization"
+df -h
