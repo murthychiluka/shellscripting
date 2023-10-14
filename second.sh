4108ba3085f3e47cb2e47a5f54caa71c0d321a2c
murthychiluka <murthychiluka@gmail.com>
2023-10-14 15:22:54 +0200

    2.1

--- second.sh
+++ second.sh
@@ -8,5 +8,5 @@ sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo wget https://www.tooplate.com/zip-templates/2098_health.zip
 sudo unzip 2098_health.zip
-sudo cp -r 2098_health.zip/* /var/www/html
+sudo cp -r 2098_health/* /var/www/html
 sudo systemctl restart httpd
