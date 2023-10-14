b0e955984699f0b2afbf943b9cd5081051941f6b
murthychiluka <murthychiluka@gmail.com>
2023-10-14 15:27:52 +0200

    2.0.1

--- second.sh
+++ second.sh
@@ -3,10 +3,12 @@
 echo "###############################"
 echo "Installing packages"
 echo "##############################"
-sudo yum install wget unzip httpd -y
+sudo yum install wget unzip httpd -y > /dev/null
 sudo systemctl start httpd
 sudo systemctl enable httpd
-sudo wget https://www.tooplate.com/zip-templates/2098_health.zip
-sudo unzip 2098_health.zip
+sudo wget https://www.tooplate.com/zip-templates/2098_health.zip > /dev/null
+sudo unzip 2098_health.zip > /dev/null
 sudo cp -r 2098_health/* /var/www/html
 sudo systemctl restart httpd
+sudo systemctl status httpd
+
