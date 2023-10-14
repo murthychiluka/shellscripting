1ddcdb2e4951183359561fe4df55e3db9a06f92c
murthychiluka <murthychiluka@gmail.com>
2023-10-14 21:51:02 +0200

    f

--- for.sh
+++ for.sh
@@ -2,8 +2,7 @@ for VAR1 in java python ruby php
 do
 echo "looping"
 echo "########################"
-echo "value of VAR1 is $VAR1
+echo "value of VAR1 is $VAR1"
 echo "########################"
 date
 done
-
