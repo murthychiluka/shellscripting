7150b04bc9a604a39254ca9d4290800b83067e3a
murthychiluka <murthychiluka@gmail.com>
2023-10-14 20:09:32 +0200

    3.0.2

---
+++ if1.sh
@@ -0,0 +1,16 @@
+read -p "Enter a number: " NUM
+if[$NUM gt 100]
+then
+echo "We have entered in IF block"
+sleep 3
+echo "Your number is greater than 100"
+echo
+date
+fi
+
+
+
+
+
+read -p "Enter your name: " NAME
+echo "Hello, $NAME!"
